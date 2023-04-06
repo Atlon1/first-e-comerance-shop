c7e9f66c5c9ac7fe857781b6358660868e891039
Atlon1 <maciej.szajstek@gmail.com>
2023-04-06 21:20:28 +0200

    let's start biuld the shop

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            Siedebar
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ src/context/CartContext.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartContext = () => {
+    return (
+        <div>
+            Cartcontxet
+        </div>
+    );
+};
+
+export default CartContext;
