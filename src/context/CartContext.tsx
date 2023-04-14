c726e1403bd1d1ae69928df4037c71972feb7ecd
Atlon1 <maciej.szajstek@gmail.com>
2023-04-14 21:22:22 +0200

    fix

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -15,7 +15,7 @@ const CartProvider = ({children}: any) => {
             return acc + currentItem.price * currentItem.amount
         }, 0)
         setTotal(total.toFixed(2))
-    }, [])
+    }, [cart])
 
     useEffect(() => {
         if (cart) {
